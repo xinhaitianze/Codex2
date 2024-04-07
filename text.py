7e56b09b968fb82bf3169758ff3f3f11f7815a0a
新海田沢 <160495636+xinhaitianze@users.noreply.github.com>
2024-04-07 21:49:28 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,7 +1,1 @@
-h = 1
-line = "abs        s"
-if 'bs' in line[:3]:
-            print(f'line {h}')
-            print(line)
-            print('^' * len(line))
-            print("ERROR: unknown instruction,  'abs' ?")
+
